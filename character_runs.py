20de7b56bd05338807343d0895dd1d7648ed85c2
gogwangsin <gwangsin1029@naver.com>
2023-09-25 09:46:38 +0900

    사진 추가

--- character_runs.py
+++ character_runs.py
@@ -3,26 +3,30 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-x = 0
+x = 750
 frame = 0
 
-def move_left(frame, x):
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+def move_left():
+    global frame, x
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
     update_canvas()
     frame = (frame + 1)%8
     x += 5 
 
 def move_right():
+    global frame, x
     character.clip_draw(frame * 100, 0, 100, 100, x, 90)
     update_canvas()
     frame = (frame + 1)%8
-    x += 5
+    x -= 5
     pass
 
 while( x < 800 ):
     clear_canvas()
     grass.draw(400,30)
-    move_left(frame, x)
+
+    move_right()
+
     delay(0.05)
     get_events()
 
