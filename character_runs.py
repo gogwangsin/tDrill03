ed07f46f14cb2227fc9648d57acad620a604f01d
gogwangsin <gwangsin1029@naver.com>
2023-09-25 09:29:09 +0900

    뭐야

--- character_runs.py
+++ character_runs.py
@@ -1,11 +1,30 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
 x = 0
+frame = 0
 
-# 여기를 채우세요.
+def move_left(frame, x):
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1)%8
+    x += 5 
+
+def move_right():
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1)%8
+    x += 5
+    pass
+
+while( x < 800 ):
+    clear_canvas()
+    grass.draw(400,30)
+    move_left(frame, x)
+    delay(0.05)
+    get_events()
 
 
 close_canvas()
